d865ebaec25efa94b69a2115237c3293f5ac6ebc
47vvt <116542696+47vvt@users.noreply.github.com>
2023-01-12 14:27:58 +1100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -5,9 +5,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-RestMethod https://get.scoop.sh | Invoke-Expression
 
 
-# requirements list
+# apps dl
 
 scoop.cmd install git aria2
 scoop.cmd bucket add extras
 scoop.cmd bucket add scutl https://github.com/47vvt/scutl
 scoop.cmd install ffmpeg yt-dlp everything scutl
+
+Write-Host @"
+Installations complete! You may close the window.
+Type 's' in Run to show scutl commands.
+"@ -ForegroundColor Green
