9f9f5c89b22db31ea3e8c776bb3beb3c4c60cf73
47vvt <116542696+47vvt@users.noreply.github.com>
2023-01-13 11:57:47 +1100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -10,9 +10,9 @@ Invoke-RestMethod https://get.scoop.sh | Invoke-Expression
 scoop.cmd install git aria2
 scoop.cmd bucket add extras
 scoop.cmd bucket add scutl https://github.com/47vvt/scutl
-scoop.cmd install ffmpeg yt-dlp everything scutl
+scoop.cmd install docto ffmpeg yt-dlp everything text-grab scutl
 
 Write-Host @"
-Installations complete! You may close the window.
+Downloads complete! You may close the window.
 Type 's' in Run to show scutl commands.
 "@ -ForegroundColor Green
